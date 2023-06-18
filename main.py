709c4f0dec61be7da86a11174fa920487c3c3b51
Replit user <>
2023-06-18 14:51:50 +0000

    main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Player:
     print(f"Health: {self.health}")
 
   def display_inventory(self):
-    print(f"Inventory : {self.inventory")
+    print(f"Inventory : {self.inventory}")
 
   def take_item(self, item):
     self.inventory.append(item)
@@ -18,28 +18,33 @@ class Player:
 
 class Game:
   def __init__(self):
-    pass
+    self.current_room
   
   @staticmethod
   def get_input(prompt):
     return input(prompt)
 
-#game on off
-game_on_off = True
+  def begginning(self):
+    #beggining story
+    print(
+    '''
+    You wake up in a cold, dimly lit dungeon with no memory of how you got there. The damp stone walls echo with eerie whispers, and a sense of foreboding fills the air. As you try to piece together what happened, you realize that escaping this mysterious place is your only hope.
+    '''
+    )
+    print("You try to gather your thoughts, but your mind feels hazy, as if a veil has been cast upon your memories. As you stand up, the metallic clank of chains around your wrists and ankles reverberates through the dungeon. It seems you're not alone in this forsaken place.")
 
-#get player's name
-player_name = input("please input your name : ")
+  def start(self):
+    #get player's name
+    player_name = input("please input your name : ")
+  
+    #make player object
+    player = Player(player_name)
+
+
 
 #make instansce class
-player = Player(player_name)
+
 game =Game()
 
-#beggining story
-print(
-'''
-You wake up in a cold, dimly lit dungeon with no memory of how you got there. The damp stone walls echo with eerie whispers, and a sense of foreboding fills the air. As you try to piece together what happened, you realize that escaping this mysterious place is your only hope.
-'''
-)
-print("You try to gather your thoughts, but your mind feels hazy, as if a veil has been cast upon your memories. As you stand up, the metallic clank of chains around your wrists and ankles reverberates through the dungeon. It seems you're not alone in this forsaken place.")
-while True:
+
   
